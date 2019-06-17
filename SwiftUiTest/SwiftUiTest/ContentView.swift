33cbf7d8a4fea5c7736c5c9ac229eb654d547fc6
Galo Guartatanga <galo.guartatanga@gmail.com>
2019-06-16 22:31:42 -0500

    integrando Swift Package Manager y SF Symbols

--- SwiftUiTest/SwiftUiTest/ContentView.swift
+++ SwiftUiTest/SwiftUiTest/ContentView.swift
@@ -7,14 +7,30 @@
 //
 
 import SwiftUI
+import Alamofire
+import AlamofireImage
 
 struct ContentView : View {
+    
+    // MARK: - Variables
+  @State private var imageIcon =  Image(
+        systemName: "link"
+    )
+    // MARK: -Body
     var body: some View {
         // declarative
         VStack (alignment: .center, spacing: 10) {
                 Image("avatar").shadow(color: .gray, radius: 10, x: 5, y: 5)
                 Text("Hola mundo").font(.title).color(.blue)
                 Text("Probando SwiftUI en Xcode 11 ").lineLimit(nil).font(.body).multilineTextAlignment(.center)
+            Button(action: {
+                Alamofire.request("https://cdn4.iconfinder.com/data/icons/logos-3/504/Swift-2-512.png").responseImage {
+                    (image) in
+                        self.imageIcon = Image(uiImage: image.result.value!)
+                }
+            }) {
+                imageIcon.resizable().frame(width:30, height: 20)
+            }.foregroundColor(.gray)
             }.padding(10)
     }
 }
